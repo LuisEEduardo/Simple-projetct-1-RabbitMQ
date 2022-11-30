61714e7312a256a8ecb20e2353faf97b9f870d6b
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-11-30 12:19:54 -0300

    Add projects publisher and consumer

---
+++ Consumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = factory.CreateConnection();
+
+            // Criar o canal na conexão para operar
+            using var channel = connection.CreateModel();
+
+            // Declaramos a fila a partir da qual vamos consumir as mensagens
+            channel.QueueDeclare
+            (
+                queue: "saudacao_1",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            // Solicita a entrega das mensagens de forma assíncrona e fornece um retorno de chamada
+            var consumer = new EventingBasicConsumer(channel);
+
+            // Recebe a mensagem da fila, converte para string e imprime o console da mensagem
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"[x] Recebida: {message}");
+            };
+
+            // Indica o consumo da mensagem
+            channel.BasicConsume
+            (
+                queue: "saudacao_1",
+                autoAck: true,
+                consumer: consumer
+            );
+
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Definindo uma conexão com um nó RabbitMQ em localhost
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            // Abrimos uma conexão com um nó RabbitMQ
+            using var connection = factory.CreateConnection();
+
+            // Criamos um canal onde vamos definir uma fila, uma mensagem e publicar a mensagem
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare
+            (
+                queue: "saudacao_1",    // nome da fila
+                durable: false,         // se igual a true a fila permanece ativa após o servidor ser reiniciado
+                exclusive: false,       // se igual a true ela só pode ser acessada via conexão atual e são excluidas ao fechar a conexão
+                autoDelete: false,      // se igual a true será deletada automaticamente após os consumidores usarem a fila
+                arguments: null
+            );
+
+            // mensagem a ser enviada
+            string message = "Welcome to RabbitMQ";
+
+            // convertendo a mensagem para um array de bytes
+            var body = Encoding.UTF8.GetBytes(message);
+
+            // Publicando a mensagem
+            channel.BasicPublish
+            (
+                exchange: "",
+                routingKey: "saudacao_1",
+                basicProperties: null,
+                body: body
+            );
+
+            Console.WriteLine($"[x] Enviada: {message}");
+        }
+    }
+}
